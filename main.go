482c14c348f51307b98c5a6a4a2bb7fde2986808
Salman Hoque <s.ulhoque@gmail.com>
2018-09-02 01:38:31 +1000

    Moved help text to a function

--- main.go
+++ main.go
@@ -19,30 +19,9 @@ type movie struct {
 
 type movies []movie
 
-var help = `
-Welcome to "Go Movie Go", You one stop movie directory.
-
-Here are some of the command you can do using this great movie app!
-
-add	
- - Add new movie name with year and rating
-list
- - List all movies
-list-by-rating
- - List all movies sorted by rating
-save
- - Save movie to a file
-read
- - Load your movies from a file
-help
- - See all commands 
-exit
- - Exit out from this app.
-`
-
 func main() {
 	var movies movies
-	fmt.Println(help)
+	help()
 
 	for {
 		action := prompt()
@@ -58,7 +37,7 @@ func main() {
 		case "read":
 			movies.readMoviesFromFile()
 		case "help":
-			fmt.Println(help)
+			help()
 		case "exit":
 			fmt.Println("\nBye Bye :)")
 			os.Exit(0)
@@ -73,6 +52,24 @@ func prompt() string {
 	fmt.Print("What you would like to do: ")
 	text, _ := reader.ReadString('\n')
 	return strings.TrimSpace(text)
+}
+
+func help() {
+	var help = `
+Welcome to "Go Movie Go", You one stop movie directory.
+
+Here are some of the command you can do using this great movie app!
+
+add:              Add new movie name with year and rating
+list:             List all movies
+list-by-rating:   List all movies sorted by rating
+save:             Save movie to a file
+read:             Load your movies from a file
+help:             See all commands 
+exit:             Exit out from this app.
+`
+
+	fmt.Println(help)
 }
 
 func (m movies) listMovies() {
