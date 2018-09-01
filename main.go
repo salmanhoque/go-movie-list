f74da8a79c62adba7e2a363fe5f5955ccf600449
Salman Hoque <s.ulhoque@gmail.com>
2018-09-02 00:33:40 +1000

    Refactored main function with functions

--- main.go
+++ main.go
@@ -73,70 +73,15 @@ func main() {
 		}
 
 		if action == "list" {
-			// Print all movies
-			fmt.Print("\n")
-			fmt.Printf("|%-30s|%-30s|%-30s|\n", "Movie Name", "Rlease Year", "Rating")
-
-			for _, movie := range movies {
-				fmt.Printf("|%-30s|%-30s|%-30.2f|\n",
-					movie.movieName, movie.releaseYear, movie.movieRating)
-			}
-
-			fmt.Print("\n")
+			listMovies(movies)
 		}
 
 		if action == "save" {
-			addReader := bufio.NewReader(os.Stdin)
-			fmt.Println("Enter a filename: ")
-			filename, _ := addReader.ReadString('\n')
-
-			file, err := os.Create(strings.TrimSpace(filename) + ".csv")
-
-			if err != nil {
-				log.Fatal("Can't create file", err)
-			}
-
-			write := csv.NewWriter(file)
-			defer write.Flush()
-
-			for _, movie := range movies {
-				ratingToStr := strconv.FormatFloat(movie.movieRating, 'f', 2, 64)
-				data := []string{movie.movieName, movie.releaseYear, ratingToStr}
-				err := write.Write(data)
-				if err != nil {
-					log.Fatal("Can't write to file", err)
-				}
-			}
-
-			println("Your movies list saved successfully!")
+			saveMovies(movies)
 		}
 
 		if action == "read" {
-			b, err := ioutil.ReadFile("test.csv")
-			if err != nil {
-				log.Fatal("Can't read the file", err)
-			}
-
-			r := csv.NewReader(strings.NewReader(string(b)))
-
-			records, err := r.ReadAll()
-			if err != nil {
-				log.Fatal("Can't read all", err)
-			}
-
-			for _, m := range records {
-				rating, _ := strconv.ParseFloat(m[2], 64)
-
-				m := movie{
-					movieName:   m[0],
-					releaseYear: m[1],
-					movieRating: rating,
-				}
-
-				movies = append(movies, m)
-			}
-
-			fmt.Println("\n Got your movies!")
+			movies = readMoviesFromFile()
 		}
 
 		if action == "exit" {
@@ -148,4 +93,73 @@ func main() {
 			fmt.Println(help)
 		}
 	}
+}
+
+func listMovies(movies []movie) {
+	fmt.Print("\n")
+	fmt.Printf("|%-30s|%-30s|%-30s|\n", "Movie Name", "Rlease Year", "Rating")
+
+	for _, movie := range movies {
+		fmt.Printf("|%-30s|%-30s|%-30.2f|\n",
+			movie.movieName, movie.releaseYear, movie.movieRating)
+	}
+
+	fmt.Print("\n")
+}
+
+func saveMovies(movies []movie) {
+	addReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a filename: ")
+	filename, _ := addReader.ReadString('\n')
+
+	file, err := os.Create(strings.TrimSpace(filename) + ".csv")
+
+	if err != nil {
+		log.Fatal("Can't create file", err)
+	}
+
+	write := csv.NewWriter(file)
+	defer write.Flush()
+
+	for _, movie := range movies {
+		ratingToStr := strconv.FormatFloat(movie.movieRating, 'f', 2, 64)
+		data := []string{movie.movieName, movie.releaseYear, ratingToStr}
+		err := write.Write(data)
+		if err != nil {
+			log.Fatal("Can't write to file", err)
+		}
+	}
+
+	println("Your movies list saved successfully!")
+}
+
+func readMoviesFromFile() []movie {
+	var movies []movie
+
+	b, err := ioutil.ReadFile("test.csv")
+	if err != nil {
+		log.Fatal("Can't read the file", err)
+	}
+
+	r := csv.NewReader(strings.NewReader(string(b)))
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal("Can't read all", err)
+	}
+
+	for _, m := range records {
+		rating, _ := strconv.ParseFloat(m[2], 64)
+
+		m := movie{
+			movieName:   m[0],
+			releaseYear: m[1],
+			movieRating: rating,
+		}
+
+		movies = append(movies, m)
+	}
+
+	fmt.Println("\n Got your movies!")
+	return movies
 }
